f2787dabca4dcf1980f4beeb3347118b9f689e92
Quin Schurman <schurmanq@gmail.com>
2023-04-19 16:30:33 -0400

    Inventory changes

--- server/main.lua
+++ server/main.lua
@@ -3,11 +3,12 @@ RPX = exports['rpx-core']:GetObject()
 RegisterNetEvent("RPXShops:SERVER:PurchaseItem", function(shop, item, amount)
     local src = source
     local player = RPX.GetPlayer(src)
+    if not player then return end
     local itemData = GetShopItem(shop, item)
     if itemData then
         local price = itemData.price * amount
         if player.money.cash >= price then
-            exports['ox_inventory']:AddItem(src, item, amount)
+            exports['rpx-inventory']:AddItem(src, item, amount)
             player.RemoveMoney('cash', price)
             TriggerClientEvent("RPXShops:CLIENT:PurchaseItem", src)
         else
